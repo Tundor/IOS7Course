c5c7cd68d75c7ba5541877205c720657e349a7c6
Manuel Müller <Manu@manuels-mbp.fritz.box>
2014-08-27 22:44:32 +0200

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Manuel Müller on 27.08.14.
+//  Copyright (c) 2014 Manuel Müller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Manuel Müller on 27.08.14.
+//  Copyright (c) 2014 Manuel Müller. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
